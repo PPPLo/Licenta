7760bd43b454aec7a0b4549424db9551a44b4436
PPPLo <65280006+PPPLo@users.noreply.github.com>
2020-05-22 16:12:55 +0300

    Welcome page Layout finished

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private matIconRegistry : MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+      this.matIconRegistry.addSvgIcon(
+        "facebook-icon",
+        this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/Icons/facebook.svg")
+      );
+      this.matIconRegistry.addSvgIcon(
+        "twitter-icon",
+        this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/Icons/twitter.svg")
+      );
+      this.matIconRegistry.addSvgIcon(
+        "instagram-icon",
+        this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/Icons/instagram.svg")
+      );
+     }
 
   ngOnInit(): void {
   }
